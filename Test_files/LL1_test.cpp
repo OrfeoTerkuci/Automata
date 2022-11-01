75deb90c0a9d81d773688e625d06fc3d16ba956f
Orfeo Terkuci <terkuciorfeo@gmail.com>
2022-11-01 13:42:55 +0100

    Added tests for LL(1) parser

---
+++ Test_files/LL1_test.cpp
@@ -0,0 +1,41 @@
+#include "../src/Context Free/CFG.h"
+
+using namespace std;
+
+int main() {
+//    CFG cfg("../Input_files/input-ll1.json");
+//    cfg.ll();
+//    CFG cfg2("../Input_files/input-ll1_2.json");
+//    cfg2.ll();
+    CFG cfg3("../Input_files/input-ll1_3.json");
+    cfg3.ll();
+    return 0;
+}
+
+/* Test 1
+FIRST: {x, }
+FOLLOW: {<EOS>, y}
+     | x        | y  | <EOS>  |
+|----|----------|----|--------|
+| S  | `x S y`  |    |        |
+|----|----------|----|--------|
+ */
+
+/* Test 2
+>>> Building LL(1) Table
+ >> FIRST:
+    A: {c, }
+    S: {a, b}
+ >> FOLLOW:
+    A: {a, b}
+    S: {<EOS>, a, b}
+>>> Table is built.
+
+-------------------------------------
+
+     | a        | b        | c      | <EOS>  |
+|----|----------|----------|--------|--------|
+| A  |          |          | `c S`  | <ERR>  |
+| S  | `a A b`  | `b A a`  | <ERR>  | <ERR>  |
+|----|----------|----------|--------|--------|
+ */
